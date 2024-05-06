8fda692d1ee2df73c1fe020add904f0b3d4f7a96
kousuke58 <147126570+kousuke58@users.noreply.github.com>
2024-05-06 17:13:47 +0900

    change colors
    all purple

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'pouple', 'pouple', 'pouple', 'pouple', 'pouple', 'pouple', 'pouple'
+    'purple', 'purple', 'purple', 'purple', 'purple', 'purple', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
